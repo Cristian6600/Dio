4ce610272d9aa589931b8de6cad74f0f004db5a1
Cristian6600 <cristian6600@hotmail.es>
2022-07-13 09:00:30 -0500

    mejoracall13-07

--- Dio/applications/call/forms.py
+++ Dio/applications/call/forms.py
@@ -222,7 +222,7 @@ class CallUpdateForm(forms.ModelForm):
 class CallGuiaUpdateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CallGuiaUpdateForm, self).__init__(*args, **kwargs)
-        self.fields['cod_vis'].queryset = Cod_vis.objects.filter(Q (id = 31)|Q(id = 32)|Q(id = 33)|Q(id=34)|Q(id=35))
+        self.fields['cod_vis'].queryset = Cod_vis.objects.filter(id = 33)
     
     class Meta:
         model = Guia
